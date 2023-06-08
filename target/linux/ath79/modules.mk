1b865df3d881d1d87db9d48022c397698c3733a1
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2023-06-08 05:11:05 -0300

    ath79: convert to module swconfig drivers
    In a first step to gradually adopt DSA switch, all swconfig modules must
    be optional, loaded as modules when needed.
    
    AT803X_PHY was also converted because it conflicts with AR8216_PHY. The
    mdio device mdio.0:00 will use the first registered driver. During the
    DSA transition, we need to have AR8216_PHY at first when swconfig is
    needed or, optionally, have AT803X_PHY missing. That way, we can control
    the order, and optionally not install, the driver needed for the
    switcher.
    
    AG71XX (ethernet driver) was also converted into a module to postpone
    mdio bus initialization. If not, mdio.0 would be registered with the
    first compatible driver, including "Generic PHY".
    
    All modules are installed by default and might be excluded on each
    device.
    
    Once swconfig/AR8216_PHY is gone from this target, both AT803X_PHY and
    AG71XX can once again become builtin.
    
    Tested with TP-Link Archer C7 v4, both in normal and failsafe.
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

--- target/linux/ath79/modules.mk
+++ target/linux/ath79/modules.mk
@@ -14,3 +14,22 @@ define KernelPackage/leds-reset/description
 endef
 
 $(eval $(call KernelPackage,leds-reset))
+
+
+NETWORK_DEVICES_MENU:=Network Devices
+
+define KernelPackage/ag71xx
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Atheros AR7XXX/AR9XXX built-in ethernet mac support
+  KCONFIG:=CONFIG_AG71XX \
+	   CONFIG_AG71XX_DEBUG_FS=y
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/atheros/ag71xx/ag71xx.ko \
+	 $(LINUX_DIR)/drivers/net/ethernet/atheros/ag71xx/ag71xx_mdio.ko
+  AUTOLOAD:=$(call AutoLoad,50,ag71xx ag71xx_mdio,1)
+endef
+
+define KernelPackage/ag71xx/description
+  Atheros AR7XXX/AR9XXX built-in ethernet mac support
+endef
+
+$(eval $(call KernelPackage,ag71xx))
